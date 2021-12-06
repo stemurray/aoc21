3052582595bbb9ae0204da78bda68594c83e968b
Stephen Murray <stephen.murray@gembaadvantage.com>
2021-12-06 09:53:02 +0000

    day 2 added

---
+++ day02/day02b.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Read file
+
+func main() {
+	movementfile, err := os.Open("./movements.txt")
+	chkerr(err)
+	defer movementfile.Close()
+
+	movements := bufio.NewScanner(movementfile)
+	movements.Split(bufio.ScanLines)
+
+	var movementlines []string
+
+	for movements.Scan() {
+		movementlines = append(movementlines, movements.Text())
+	}
+
+	forward := 0
+	depth := 0
+	aim := 0
+
+	for _, move := range movementlines {
+
+		// Split into type and amount
+
+		words := strings.Fields(move)
+
+		movetype := words[0]
+		moveamountint, err := strconv.Atoi(words[1])
+		chkerr(err)
+
+		switch movetype {
+		case "forward":
+			forward += moveamountint
+			depth = depth + (aim * moveamountint)
+		case "down":
+			aim += moveamountint
+		case "up":
+			aim -= moveamountint
+		}
+	}
+
+	fmt.Println(forward * depth)
+
+}
+
+// for loop through lines checking if greater than previous
+
+// what is starting value
+// how best to store current value between iterations
+
+// print answer
+
+//Functions
+
+func chkerr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
