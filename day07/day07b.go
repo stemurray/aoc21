6b74641876aecf6973f7ce2f6a5690cbce924d3b
Stephen Murray <stephen.murray@gembaadvantage.com>
2021-12-10 22:54:03 +0000

    day07

---
+++ day07/day07b.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	// Open file and read as words without whitespace
+	crabfile, err := os.Open("./crabs.txt")
+	chkerr(err)
+	defer crabfile.Close()
+
+	crabscan := bufio.NewScanner(crabfile)
+	crabscan.Split(bufio.ScanWords)
+
+	// Read first line (which is comma separated) and add to a slice of strings
+	crabscan.Scan()
+	crabstr := crabscan.Text()
+	crabslc := strings.Split(crabstr, ",")
+	var crabs []int
+
+	for _, c := range crabslc {
+		crabint, err := strconv.Atoi(c)
+		chkerr(err)
+		crabs = append(crabs, crabint)
+	}
+
+	// fmt.Println(crabs)
+
+	var lowestinitialposition int
+	var highestinitialposition int
+
+	for _, p := range crabs {
+		if p < lowestinitialposition {
+			lowestinitialposition = p
+		}
+		if p > highestinitialposition {
+			highestinitialposition = p
+		}
+	}
+
+	var fuel int
+
+	for _, p := range crabs {
+		fuel = fuel + cost(Abs(lowestinitialposition-p))
+	}
+
+	for t := lowestinitialposition + 1; t < highestinitialposition+1; t++ {
+
+		var f int
+
+		for _, p := range crabs {
+			f = f + cost(Abs(t-p))
+		}
+
+		if f < fuel {
+			fuel = f
+		}
+	}
+
+	fmt.Println(fuel)
+
+}
+
+//Functions
+
+func chkerr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func cost(x int) int {
+	var cst int
+	for i := 1; i < x+1; i++ {
+		cst = cst + i
+	}
+	return cst
+}
